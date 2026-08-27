a1ff761f4ca100bd1d1c6d34dc338da764b69678
NewNoob2002 <guptianci@gmail.com>
2026-08-27 10:17:10 +0800

    fix(usb): service external watchdog in loopback

--- App/Core/Src/usb_vendor_bulk_main.c
+++ App/Core/Src/usb_vendor_bulk_main.c
@@ -1,14 +1,18 @@
 #include "bsp_clock.h"
+#include "bsp_external_watchdog.h"
+#include "hc32_ll_utility.h"
 #include "hc32f460.h"
 #include "usb_vendor_bulk.h"
 
 volatile int g_usb_vendor_bulk_init_result;
 
-int main(void)
-{
+int main(void) {
+    uint32_t now_ms = 0U;
+
     bsp_clock_init();
-    g_usb_vendor_bulk_init_result = usb_vendor_bulk_init();
+    g_usb_vendor_bulk_init_result = bsp_external_watchdog_init(now_ms) ? usb_vendor_bulk_init() : -1;
     for (;;) {
-        __WFI();
+        DDL_DelayMS(1U);
+        bsp_external_watchdog_poll(++now_ms);
     }
 }
