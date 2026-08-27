b3da6133e4cd17c13c9815f65cd99a67d8280781
NewNoob2002 <guptianci@gmail.com>
2026-08-27 11:31:59 +0800

    fix(usb): flush endpoint FIFOs on bus reset

--- Platform/HC32F460/Src/usb_dc_hc32f460.c
+++ Platform/HC32F460/Src/usb_dc_hc32f460.c
@@ -254,7 +254,8 @@ static void usb_handle_out_ep(void)
 static void usb_handle_reset(void)
 {
     CLR_REG32_BIT(g_usb.regs.DREGS->DCTL, USBFS_DCTL_RWUSIG);
-    usb_txfifoflush(&g_usb.regs, 0UL);
+    usb_txfifoflush(&g_usb.regs, 0x10UL);
+    usb_rxfifoflush(&g_usb.regs);
     for (uint8_t i = 0U; i < g_usb.basic.dev_epnum; ++i) {
         WRITE_REG32(g_usb.regs.INEP_REGS[i]->DIEPINT, 0xFFUL);
         WRITE_REG32(g_usb.regs.OUTEP_REGS[i]->DOEPINT, 0xFFUL);
